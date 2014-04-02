ef75a08176e8e71177bf1e6e79db6553e5cbb320
doodle <doodlehp@gmail.com>
2014-04-02 18:07:37 +0300

    Cleaned up the quaternion unit test

--- test/renderTest/QuaternionTest.cpp
+++ test/renderTest/QuaternionTest.cpp
@@ -7,41 +7,53 @@ namespace
 TEST(QuaternionAxisAngle)
 {
     using namespace der;
-    Quaternion q1;
 
-    Vector3 v(1.0f, 2.0f, 3.0f);
-    q1.rotation_from_axis_angle(v, Math::PI);
+    Vector3 axis(1.0f, 2.0f, 3.0f);
+    axis.normalize();
+    const float angle = Math::PI * 0.33f;
 
-    Vector3 axis; float theta;
-    q1.get_axis_angle(axis.x, axis.y, axis.z, theta);
-    CHECK_EQUAL(true, axis.equals(v));
-    CHECK_EQUAL(true, equals(theta, Math::PI));
+    Quaternion q;
+    q.rotation_from_axis_angle(axis, angle);
+
+    Vector3 axis2;
+    float angle2;
+    q.get_axis_angle(axis2, angle2);
+    CHECK_EQUAL(axis, axis2);
+    CHECK_CLOSE(angle, angle2, Math::EPSILON);
 }
 
 
 TEST(QuaternionSlerp)
 {
     using namespace der;
+
     Quaternion q1, q2;
     q1.rotation_from_axis_angle(1.0f, 2.0f, 3.0f, Math::PI);
     q2.rotation_from_axis_angle(0.5f, 1.0f, 0.0f, Math::PI);
 
     Quaternion result = slerp(q1, q2, 0.0f);
-    CHECK_EQUAL(true, q1.equals(result));
+    CHECK_EQUAL(q1, result);
 
+    // As CHECK_EQUAL uses == operator, we can't check for inequality directly.
+    // Also, the math primitives' == and != operators use epsilon comparison.
     result = slerp(q1, q2, 0.0f);
-    CHECK_EQUAL(false, q2.equals(result));
+    CHECK_EQUAL(true, q2 != result);
 
     result = slerp(q1, q2, 1.0f);
-    CHECK_EQUAL(true, q2.equals(result));
+    CHECK_EQUAL(q2, result);
+
+    // Slerp between rotations around same angle.
+    q1.rotation_from_axis_angle(1.0f, 0.0f, 0.0f, Math::PI / 4);
+    q2.rotation_from_axis_angle(1.0f, 0.0f, 0.0f, 0.0f);
+    result = slerp(q1, q2, 0.5f);
 
     Quaternion expected_q;
-    q1.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, Math::PI / 4);
-    q2.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, 0.0f);
-    result = slerp(q1, q2, 0.5f);
-    expected_q.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, (Math::PI / 4) * 0.5f);
+    expected_q.rotation_from_axis_angle(1.0f, 0.0f, 0.0f, (Math::PI / 4) * 0.5f);
     CHECK_EQUAL(expected_q, result);
 
+
+    // Again, slerp between rotations around same angle.
+    q1.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, Math::PI / 4);
     q2.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, Math::PI / 2);
     const float t = 0.58f;
     result = slerp(q1, q2, t);
@@ -49,6 +61,7 @@ TEST(QuaternionSlerp)
 
     expected_q.rotation_from_axis_angle(0.0f, 1.0f, 0.0f, expected_angle);
     CHECK_EQUAL(expected_q, result);
+
 
     Vector3 axis; float angle;
     result.get_axis_angle(axis, angle);

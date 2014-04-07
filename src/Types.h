83b3b5e7c7c057409da5958ba1a007af3eebe0d9
doodle <doodlehp@gmail.com>
2014-04-07 23:35:53 +0300

    Mesh implementation and Types.h

---
+++ src/Types.h
@@ -0,0 +1,15 @@
+
+#ifndef H_DER_TYPES_H
+#define H_DER_TYPES_H
+
+#include <cinttypes>
+
+namespace der
+{
+
+    typedef unsigned int size_t;
+
+} // der
+
+#endif // H_DER_TYPES_H
+
---
+++ src/renderer/Mesh.cpp
@@ -0,0 +1,51 @@
+
+#include "Mesh.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "../Debug.h"
+
+namespace der
+{
+
+    Mesh::Mesh()
+        : m_vertex_buffer(nullptr)
+        , m_index_buffer(nullptr)
+        , m_submesh_count(0)
+    { }
+
+    Mesh::~Mesh()
+    {
+        delete m_vertex_buffer;
+        delete m_index_buffer;
+    }
+
+    void Mesh::set_buffers(VertexBuffer *vb, IndexBuffer *ib)
+    {
+        m_vertex_buffer = vb;
+        m_index_buffer = ib;
+    }
+
+    VertexBuffer* Mesh::get_vertex_buffer()
+    { return m_vertex_buffer; }
+
+    IndexBuffer* Mesh::get_index_buffer()
+    { return m_index_buffer; }
+
+    void Mesh::add_submesh(size_t start_index, size_t index_count)
+    {
+        DER_ASSERT(m_submesh_count < MAX_SUB_MESHES);
+        m_submeshes[m_submesh_count].m_start_index = start_index;
+        m_submeshes[m_submesh_count].m_index_count = index_count;
+        m_submesh_count++;
+    }
+
+    const SubMesh& Mesh::get_submesh(size_t index) const
+    {
+        DER_ASSERT(index < m_submesh_count);
+        return m_submeshes[index];
+    }
+
+    size_t Mesh::get_submesh_count() const
+    { return m_submesh_count; }
+
+} // der

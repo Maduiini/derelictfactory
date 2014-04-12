e93ee0570869e06db3c07dcee66c5065ab33cfd5
doodle <doodlehp@gmail.com>
2014-04-12 19:55:06 +0300

    Improved make_resouce() hashing method

--- src/resource/Resource.h
+++ src/resource/Resource.h
@@ -9,15 +9,19 @@ namespace der
 
     typedef size_t ResourceID;
 
+    constexpr uint32_t rotate(uint32_t value, uint32_t rot)
+    {
+        return (value << rot) + (value >> (32 - rot));
+    }
 
     constexpr uint32_t make_resource_r(const char * const str, uint32_t h)
     {
-        return (!*str) ? h : make_resource_r(str + 1, *str + h + (h << 5));
+        return (!*str) ? h : make_resource_r(str + 1, *str + h + rotate(h, 5));
     }
 
     constexpr ResourceID make_resource(const char * const name)
     {
-        return static_cast<ResourceID>(make_resource_r(name, 1234));
+        return static_cast<ResourceID>(make_resource_r(name, 0x1234));
     }
 
 } // der

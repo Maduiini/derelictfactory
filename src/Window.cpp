1eae7c653e7e550df1a3fc01c6ed24880c9a0c1f
doodle <doodlehp@gmail.com>
2014-04-22 22:05:54 +0300

    Added keyboard and mouse input checking methods to Window

--- src/Window.cpp
+++ src/Window.cpp
@@ -13,12 +13,38 @@ namespace der
         wnd->on_resize(w, h);
     }
 
+    void on_window_key(GLFWwindow *window, int key, int scancode, int action, int mods)
+    {
+        Window *wnd = static_cast<Window*>(::glfwGetWindowUserPointer(window));
+        wnd->on_key(key, scancode, action, mods);
+    }
+
+    void on_window_mouse_button(GLFWwindow *window, int button, int action, int mods)
+    {
+        Window *wnd = static_cast<Window*>(::glfwGetWindowUserPointer(window));
+        wnd->on_mouse_button(button, action, mods);
+    }
+
+    void on_window_mouse_move(GLFWwindow *window, double x, double y)
+    {
+        Window *wnd = static_cast<Window*>(::glfwGetWindowUserPointer(window));
+        wnd->on_mouse_move(x, y);
+    }
+
 
     Window::Window()
         : m_window(nullptr)
         , m_monitor(nullptr)
         , m_has_resized(true)
-    { }
+        , m_mouse_x(0.0f)
+        , m_mouse_y(0.0f)
+        , m_mouse_dx(0.0f)
+        , m_mouse_dy(0.0f)
+        , m_mouse_captured(false)
+    {
+        for (size_t i = 0; i < KEY_COUNT; i++) m_keys[i] = GLFW_RELEASE;
+        for (size_t i = 0; i < BUTTON_COUNT; i++) m_mouse_button[i] = GLFW_RELEASE;
+    }
 
     Window::~Window()
     {
@@ -58,7 +84,11 @@ namespace der
         }
 
         ::glfwSetWindowUserPointer(m_window, static_cast<void*>(this));
+
         ::glfwSetWindowSizeCallback(m_window, &on_window_resize);
+        ::glfwSetKeyCallback(m_window, &on_window_key);
+        ::glfwSetMouseButtonCallback(m_window, &on_window_mouse_button);
+        ::glfwSetCursorPosCallback(m_window, &on_window_mouse_move);
 
         int major = ::glfwGetWindowAttrib(m_window, GLFW_CONTEXT_VERSION_MAJOR);
         int minor = ::glfwGetWindowAttrib(m_window, GLFW_CONTEXT_VERSION_MINOR);
@@ -103,8 +133,13 @@ namespace der
         return m_monitor != nullptr;
     }
 
-    void Window::poll_events() const
+    void Window::poll_events()
     {
+        m_mouse_dx = 0;
+        m_mouse_dy = 0;
+        for (size_t i = 0; i < KEY_COUNT; i++)
+            if (m_keys[i] == GLFW_PRESS) m_keys[i] = GLFW_REPEAT;
+
         ::glfwPollEvents();
     }
 
@@ -140,7 +175,80 @@ namespace der
         return resized;
     }
 
+    bool Window::key_pressed(Key key) const
+    { return m_keys[static_cast<int>(key)] == GLFW_PRESS; }
+
+    bool Window::key_down(Key key) const
+    { return m_keys[static_cast<int>(key)] != GLFW_RELEASE; }
+
+    bool Window::button_down(MouseButton button) const
+    { return m_mouse_button[static_cast<int>(button)] == GLFW_PRESS; }
+
+    float Window::get_mouse_x() const
+    { return m_mouse_x; }
+
+    float Window::get_mouse_y() const
+    { return m_mouse_y; }
+
+    float Window::get_mouse_dx() const
+    { return m_mouse_dx; }
+
+    float Window::get_mouse_dy() const
+    { return m_mouse_dy; }
+
+
+    void Window::set_mouse_captured(bool captured)
+    {
+        m_mouse_captured = captured;
+
+        const int value = m_mouse_captured ?
+            GLFW_CURSOR_HIDDEN : GLFW_CURSOR_NORMAL;
+        ::glfwSetInputMode(m_window, GLFW_CURSOR, value);
+    }
+
+    void Window::toggle_mouse_captured()
+    { set_mouse_captured(!m_mouse_captured); }
+
+    bool Window::is_mouse_captured() const
+    { return m_mouse_captured; }
+
+
+
     void Window::on_resize(int w, int h)
     { m_has_resized = true; }
+
+    void Window::on_key(int key, int scancode, int action, int mods)
+    {
+        if (size_t(key) < KEY_COUNT)
+        {
+            m_keys[key] = action;
+        }
+    }
+
+    void Window::on_mouse_button(int button, int action, int mods)
+    {
+        m_mouse_button[button] = action;
+    }
+
+    void Window::on_mouse_move(double x, double y)
+    {
+        float last_x = m_mouse_x;
+        float last_y = m_mouse_y;
+        m_mouse_x = x;
+        m_mouse_y = y;
+        m_mouse_dx = m_mouse_x - last_x;
+        m_mouse_dy = m_mouse_y - last_y;
+
+        if (m_mouse_captured)
+        {
+            int w, h;
+            get_size(&w, &h);
+            const double xpos = 0.5 * w;
+            const double ypos = 0.5 * h;
+            ::glfwSetCursorPos(m_window, xpos, ypos);
+            m_mouse_x = xpos;
+            m_mouse_y = ypos;
+        }
+    }
 
 } // der
--- src/Window.h
+++ src/Window.h
@@ -2,6 +2,9 @@
 #ifndef H_DER_WINDOW_H
 #define H_DER_WINDOW_H
 
+#include "Input.h"
+#include "Types.h"
+
 struct GLFWwindow;
 struct GLFWmonitor;
 
@@ -25,7 +28,7 @@ namespace der
 //        void set_fullscreen(bool fullscreen);
         bool is_fullscreen() const;
 
-        void poll_events() const;
+        void poll_events();
 
         bool should_close() const;
 
@@ -36,15 +39,45 @@ namespace der
 
         bool has_resized();
 
+        bool key_pressed(Key key) const;
+        bool key_down(Key key) const;
+        bool button_down(MouseButton button) const;
+        float get_mouse_x() const;
+        float get_mouse_y() const;
+        float get_mouse_dx() const;
+        float get_mouse_dy() const;
+
+        void set_mouse_captured(bool captured);
+        void toggle_mouse_captured();
+        bool is_mouse_captured() const;
+
     private:
         friend void on_window_resize(GLFWwindow*, int, int);
+        friend void on_window_key(GLFWwindow*, int, int, int, int);
+        friend void on_window_mouse_button(GLFWwindow*, int, int, int);
+        friend void on_window_mouse_move(GLFWwindow*, double, double);
 
         void on_resize(int w, int h);
+        void on_key(int key, int scancode, int action, int mods);
+        void on_mouse_button(int button, int action, int mods);
+        void on_mouse_move(double x, double y);
 
     private:
         GLFWwindow *    m_window;
         GLFWmonitor *   m_monitor;
         bool            m_has_resized;
+
+        static const size_t KEY_COUNT = 348; // The last GLFW_KEY_* is 348
+        int m_keys[KEY_COUNT];
+
+        static const size_t BUTTON_COUNT = 8;
+        int m_mouse_button[BUTTON_COUNT];
+        float m_mouse_x;
+        float m_mouse_y;
+        float m_mouse_dx;
+        float m_mouse_dy;
+
+        bool m_mouse_captured;
     };
 
 } // der

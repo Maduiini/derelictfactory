f5a1aa0072cdb6edfaa824ad54b0d958b6f4a364
doodle <doodlehp@gmail.com>
2014-04-27 20:56:25 +0300

    Adjusted the camera move speed

--- src/input/CameraController.cpp
+++ src/input/CameraController.cpp
@@ -11,13 +11,15 @@ namespace der
         , m_rot_y(0.0f)
     { }
 
-
     void CameraController::update(float delta_time)
     {
         const Matrix3 rot = m_object->get_rotation_matrix();
 
         Vector3 right, up, forward;
         rot.get_basis(right, up, forward);
+
+        const float move_speed = 2.5f;
+        const float rotate_speed = 1.0f;
 
         Vector3 delta = Vector3::zero;
 
@@ -34,14 +36,14 @@ namespace der
         if (key_down(Key::Left_Control))
             delta -= up;
 
-        m_object->move(delta * delta_time);
+        m_object->move(delta * move_speed * delta_time);
 
         if (key_pressed(Key::Left_Alt))
             toggle_mouse_captured();
 
         if (is_mouse_captured())
         {
-            const Vector2 mouse_delta = get_mouse_delta() * delta_time;
+            const Vector2 mouse_delta = get_mouse_delta() * rotate_speed * delta_time;
             m_rot_x += mouse_delta.y;
             m_rot_y += mouse_delta.x;
         }

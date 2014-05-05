b235f0b45d866173bdf8df0d31ee6491d45204cb
maqqr <maqqr@server.fake>
2014-05-05 19:17:26 +0300

    Enabled anti-aliasing

--- src/Window.cpp
+++ src/Window.cpp
@@ -71,6 +71,7 @@ namespace der
         ::glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         //::glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         //::glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        ::glfwWindowHint(GLFW_SAMPLES, 8);
 
         #ifdef DER_DEBUG
         ::glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);

0b3355d6eb1a5137fef92f4210fca2be7b48d042
doodle <doodlehp@gmail.com>
2014-05-06 14:54:21 +0300

    Added default max texture anisotropy to 16 (16x anisotropic filtering)

--- src/renderer/Texture.cpp
+++ src/renderer/Texture.cpp
@@ -69,6 +69,7 @@ namespace der
         // Set filtering options. These should be set in bind() method when ever they are changed.
         ::glTexParameteri(m_target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         ::glTexParameteri(m_target, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        ::glTexParameteri(m_target, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
 
         // Upload the texture image data
         ::glTexImage2D(m_target, 0, internal_fmt, w, h, 0, gl_fmt, gl_type, data);
